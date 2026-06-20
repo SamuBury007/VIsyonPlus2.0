27416963b4768019703d4868f0a67118b708317d
SamuBury007 <samybury07@gmail.com>
2026-06-20 15:50:52 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,3 @@
 #!/usr/bin/env bash
 set -e
-
-echo "[build] Installazione dipendenze Python..."
 pip install -r requirements.txt
-
-echo "[build] Installazione Chromium per Playwright..."
-playwright install chromium
-
-echo "[build] Done."
