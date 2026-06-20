af253e4c0dfc69a26b677a9cfea033fd8c73c009
SamuBury007 <samybury07@gmail.com>
2026-06-20 14:19:10 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 set -e
-pip install -r requirements.txt
 
-# Installa dipendenze di sistema senza sudo (già disponibili su Render)
 export PLAYWRIGHT_BROWSERS_PATH=/opt/render/.cache/ms-playwright
 
-# Scarica solo il browser, senza tentare install-deps
+pip install -r requirements.txt
 playwright install chromium
